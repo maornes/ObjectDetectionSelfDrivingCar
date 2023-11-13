4d2585b301a2b34365468c49fa6349a5112e9698
maornes <132849820+maornes@users.noreply.github.com>
2023-11-13 18:25:01 +0100

    Update train.py

--- train.py
+++ train.py
@@ -21,15 +21,15 @@ n_classes = len(label_map)  # number of different types of objects
 
 # Learning parameters
 checkpoint = None  # path to model checkpoint, None if none
-batch_size = 16  # batch size TODO
-iterations = 100 #120000  # number of iterations to train TODO
-workers = 4  # number of workers for loading data in the DataLoader TODO
-print_freq = 200  # print training status every __ batches TODO
-lr = 1e-3  # learning rate TODO
-decay_lr_at = [80000, 100000]  # decay learning rate after these many iterations TODO
-decay_lr_to = 0.1  # decay learning rate to this fraction of the existing learning rate TODO
-momentum = 0.9  # momentum TODO
-weight_decay = 5e-4  # weight decay TODO
+batch_size = 16  # batch size
+iterations = 100 #120000  # number of iterations to train
+workers = 4  # number of workers for loading data in the DataLoader 
+print_freq = 200  # print training status every __ batches 
+lr = 1e-3  # learning rate 
+decay_lr_at = [80000, 100000]  # decay learning rate after these many iterations 
+decay_lr_to = 0.1  # decay learning rate to this fraction of the existing learning rate 
+momentum = 0.9  # momentum 
+weight_decay = 5e-4  # weight decay 
 grad_clip = None  # clip if gradients are exploding, which may happen at larger batch sizes (sometimes at 32) - you will recognize it by a sorting error in the MuliBox loss calculation
 
 cudnn.benchmark = True
@@ -71,7 +71,7 @@ def main():
     # To convert iterations to epochs, divide iterations by the number of iterations per epoch
     # The paper trains for 120,000 iterations with a batch size of 32, decays after 80,000 and 100,000 iterations
     #epochs = iterations // (len(train_dataset) // 32)
-    epochs = 5
+    epochs = 2
     decay_lr_at = [it // (len(train_dataset) // 32) for it in decay_lr_at]
 
     # Epochs
@@ -91,6 +91,7 @@ def main():
         # Save checkpoint
         save_checkpoint(epoch, model, optimizer)
 
+
 def train(train_loader, model, criterion, optimizer, epoch):
     """
     One epoch's training.
@@ -109,49 +110,49 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
     start = time.time()
     
-    train_losses = []
+    #train_losses = []
 
     # Batches
-    i = 0
-    images, boxes, labels = next(iter(train_loader)) 
-    data_time.update(time.time() - start)
+    for i, batch in enumerate(train_loader):
+        images, boxes, labels = next(iter(train_loader)) 
+        data_time.update(time.time() - start)
 
         # Move to default device
-    images = images.to(device)  # (batch_size (N), 3, 300, 300)
-    boxes = [b.to(device) for b in boxes]
-    labels = [l.to(device) for l in labels]
+        images = images.to(device)  # (batch_size (N), 3, 300, 300)
+        boxes = [b.to(device) for b in boxes]
+        labels = [l.to(device) for l in labels]
 
         # Forward prop.
-    predicted_locs, predicted_scores = model(images)  # (N, 8732, 4), (N, 8732, n_classes)
+        predicted_locs, predicted_scores = model(images)  # (N, 8732, 4), (N, 8732, n_classes)
 
         # Loss
-    loss = criterion(predicted_locs, predicted_scores, boxes, labels)  # scalar 
-    train_losses.append(loss.item())
+        loss = criterion(predicted_locs, predicted_scores, boxes, labels)  # scalar 
+        #train_losses.append(loss.item())
 
         # Backward prop.
-    optimizer.zero_grad()
-    loss.backward()
+        optimizer.zero_grad()
+        loss.backward()
 
         # Clip gradients, if necessary
-    if grad_clip is not None:
-        clip_gradient(optimizer, grad_clip)
+        if grad_clip is not None:
+            clip_gradient(optimizer, grad_clip)
 
         # Update model
-    optimizer.step()
+        optimizer.step()
 
-    losses.update(loss.item(), images.size(0))
-    batch_time.update(time.time() - start)
+        losses.update(loss.item(), images.size(0))
+        batch_time.update(time.time() - start)
 
-    start = time.time()
+        start = time.time()
 
         # Print status
-    if i % print_freq == 0:
-        print('Epoch: [{0}][{1}/{2}]\t'
-                'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                'Data Time {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, i, len(train_loader),
-                                                                  batch_time=batch_time,
-                                                                  data_time=data_time, loss=losses))
+        if i % print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                    'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                    'Data Time {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                    'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, i, len(train_loader),
+                                                                      batch_time=batch_time,
+                                                                      data_time=data_time, loss=losses))
     del predicted_locs, predicted_scores, images, boxes, labels  # free some memory since their histories may be stored
     
     #plot_loss(train_losses)#, val_losses)
